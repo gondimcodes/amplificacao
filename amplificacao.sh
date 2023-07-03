fd09a0534333945fb52413dbd7b5e2023ed3d99b
root <root@gondim.localdomain>
2023-07-03 12:32:25 -0300

    bug correction

--- amplificacao.sh
+++ amplificacao.sh
@@ -4,7 +4,7 @@
 # pelo uso desse script.
 # Autor: Marcelo Gondim - gondim at gmail.com
 # Data: 18/02/2023
-# Versao: 2.5
+# Versao: 2.6
 #
 vermelho='\033[0;31m'
 verde='\033[0;32m'
@@ -44,7 +44,7 @@ echo -e "Testando NETBIOS (137/udp): \c"
 if [ "`nmblookup -A $1 | grep -i \"No reply from\"`" == "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -57,7 +57,7 @@ echo -e "Testando DVR-DHCPDiscover (37810/udp): \c"
 if [ "`echo -n \"\\xff\" | nc -w 3 -u $1 37810 | hexdump -C`" != "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -70,7 +70,7 @@ echo -e "Testando RPC (111/udp): \c"
 if [ "`rpcinfo -T udp -p $1 2> /dev/null`" != "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -83,7 +83,7 @@ echo -e "Testando ARMS (3283/udp): \c"
 if [ "`printf \"\\x00\\x14\\x00\\x01\\x03\" | nc -w 3 -u $1 3283 | hexdump -C`" != "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -96,7 +96,7 @@ echo -e "Testando TFTP (69/udp): \c"
 if [ "`curl -m 3 tftp://$1/a.pdf 2>&1 | grep -i \"File Not Found\"`" != "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -113,7 +113,7 @@ if [ -z "`host -W 5 google.com $1|grep -i \"connection timed out\"`" ]; then
       echo -e "${verde}Fechada${semcor}"
    fi
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -126,7 +126,7 @@ echo -e "Testando Multicast DNS (5353/udp): \c"
 if [ "`dig +timeout=1 @$1 -p 5353 ptr _services._dns-sd._udp.local | grep -i \"connection timed out\"`" == "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -139,7 +139,7 @@ echo -e "Testando SSDP (1900/udp): \c"
 if [ "`printf \"M-SEARCH * HTTP/1.1\\r\\nHost:239.255.255.250:1900\\r\\nST:upnp:rootdevice\\r\\nMan:\\"ssdp:discover\\"\\r\\nMX:3\\r\\n\\r\\n\" | nc -w 3 -u $1 1900 | hexdump -C`" != "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -152,7 +152,7 @@ echo -e "Testando SNMP (161/udp): \c"
 if [ "`snmpget -v 2c -c public $1 iso.3.6.1.2.1.1.1.0 2> /dev/null`" != "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -165,7 +165,7 @@ echo -e "Testando NTP (123/udp): \c"
 if [ "`ntpq -c rv $1 2>&1 | grep -i \"timed out\"`" == "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -178,7 +178,7 @@ echo -e "Testando LDAP (389/udp): \c"
 if [ "`ldapsearch -x -h $1 -s base 2> /dev/null | hexdump -C`" != "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -191,7 +191,7 @@ echo -e "Testando UBNT (10001/udp): \c"
 if [ "`printf  \"\\x01\\x00\\x00\\x00\" | nc -w 3 -u $1 10001 | hexdump -C`" != "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -204,7 +204,7 @@ echo -e "Testando CHARGEN (19/udp): \c"
 if [ "`echo | nc -w 1 -u $1 19 | hexdump -C`" != "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -217,7 +217,7 @@ echo -e "Testando QOTD (17/udp): \c"
 if [ "`echo | nc -w 1 -u $1 17 | hexdump -C`" != "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -230,7 +230,7 @@ echo -e "Testando MEMCACHED (11211/udp): \c"
 if [ "`printf '\\x0\\x0\\x0\\x0\\x0\\x1\\x0\\x0\\x73\\x74\\x61\\x74\\x73\\x0a' | nc -w 3 -u $1 11211 | hexdump -C`" != "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -243,7 +243,7 @@ echo -e "Testando WS-DISCOVERY (3702/udp): \c"
 if [ "`printf '<\\252>\\n' | nc -w 3 -u $1 3702 | hexdump -C`" != "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -256,7 +256,7 @@ echo -e "Testando CoAP (5683/udp): \c"
 if [ "`printf '\\x40\\x01\\x7d\\x70\\xbb\\x2e\\x77\\x65\\x6c\\x6c\\x2d\\x6b\\x6e\\x6f\\x77\\x6e\\x04\\x63\\x6f\\x72\\x65' | nc -w3 -u $1 5683| hexdump -C`" != "" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -269,7 +269,7 @@ echo -e "Testando MT4145 (4145/tcp): \c"
 if [ "`nmap -sT -pT:4145 -Pn -n $1|grep open|awk '{print $2}'`" == "open" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
@@ -282,7 +282,7 @@ echo -e "Testando MT5678 - botnet Meris (5678/tcp): \c"
 if [ "`nmap -sT -pT:5678 -Pn -n $1|grep open|awk '{print $2}'`" == "open" ]; then
    echo -e "${vermelho}Aberta${semcor}"
 else
-   if [ "`fping $1 | grep -i 'alive'`" != "" ]; then
+   if [ "`fping $1 2> /dev/null | grep -i 'alive'`" != "" ]; then
       echo -e "${verde}Fechada${semcor}"
    else
       echo -e "${azul}Inconclusivo${semcor}"
